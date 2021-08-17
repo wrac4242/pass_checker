b943298056034b4c1985d868238853a3598e3310
wrac4242 <wrac4242@gmail.com>
2021-08-17 20:07:00 +0100

    now loads all the passwords

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-const useragent = "wrac passchecker";
+const userAgent = "wrac passchecker";
 const baseURL = "https://api.pwnedpasswords.com/range/";
+const fs = require('fs');
+var sha1 = require('sha1');
 
+function store(password) {
+	let hash = sha1(password);
+	let fivChar = hash.substring(0, 6);
+	return {
+		hash: hash,
+		fiv: fivChar,
+		password: password
+	}
+};
+
+const passwordFiles = fs.readdirSync('./passwords').filter(file => file.endsWith('.json'));
+
+const passwords = [];
+
+for (const file of passwordFiles) {
+	const vault = require(`./passwords/${file}`);
+	vaultProcess(vault);
+}
+
+function vaultProcess(vault) {
+	for (const items of vault.items) {
+		let pass = items.login.password;
+		if (pass) {
+			passwords.push(store(pass));
+		}
+	}
+}
+
+console.log(passwords);
