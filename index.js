e2a4f468f47167bc6fd3866ac67615f28ad918d6
wrac4242 <wrac4242@gmail.com>
2021-08-18 21:47:43 +0100

    almost works

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const https = require('https')
 
 function store(password) {
 	let hash = sha1(password);
-	let fivChar = hash.substring(0, 6);
+	let fivChar = hash.substring(0, 5);
 	return {
-		hash: hash.substring(6),
+		hash: hash.substring(5),
 		fiv: fivChar,
 		password: password
 	}
@@ -33,6 +33,8 @@ function vaultProcess(vault) {
 // function returns a Promise
 function getPromise(hash) {
 	return new Promise((resolve, reject) => {
+		let hash = hash;
+		console.log(`https://api.pwnedpasswords.com/range/${hash}`);
 		https.get(`https://api.pwnedpasswords.com/range/${hash}`, (response) => {
 			let chunks_of_data = [];
 
@@ -57,6 +59,7 @@ async function makeSynchronousRequest(obj) {
 	try {
 		let https_promise = getPromise(obj.fiv);
 		let response_body = await https_promise;
+		console.log(response_body);
 
 		// holds response from server that is passed when Promise is resolved
 		return response_body.includes(obj.hash)
@@ -69,14 +72,24 @@ async function makeSynchronousRequest(obj) {
 
 // for each password, send the request, and check if its in there
 
+const breachedPasswords = [];
+
 async function checkPasswd(pass_obj) {
 	// returns if its there or not
 	let result = await makeSynchronousRequest(pass_obj);
-	return result;
+	if (result) {
+		breachedPasswords.push(pass_obj);
+	};
 }
 
-let log = await checkPasswd({
-	fiv: "21BD1",
-	hash: "00D4F6E8FA6EECAD2A3AA415EEC418D38EC",
-	password: "test"
-});
+const promiseArray = [];
+
+for (i in passwords) {
+	promiseArray.push(checkPasswd(i));
+}
+
+(async () => await Promise.all(promiseArray))();
+
+for (i in breachedPasswords) {
+	console.log(i.password);
+}
